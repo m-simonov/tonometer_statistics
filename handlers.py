c08364e869b5295d9a5d3502fd7417e682ef1925
simonovmaksim <simonov.maksim13@yandex.ru>
2021-10-21 22:03:13 +0300

    Added user record to db

--- handlers.py
+++ handlers.py
@@ -1,4 +1,9 @@
+import sqlite3
+import datetime
+import pytz
+
 from aiogram import types
+
 from main import bot, dp
 
 
@@ -6,3 +11,15 @@ from main import bot, dp
 async def print_info(message: types.Message):
     text = "Hello"
     await message.answer(text=text)
+
+    base = sqlite3.connect('tonometer_results.db')
+    cur = base.cursor()
+
+    tid = message.from_user.id
+    user_name = message.from_user.username
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    base.execute('CREATE TABLE IF NOT EXISTS users(tid PRIMARY KEY, username, fitst_name, last_name)')
+    cur.execute('INSERT OR IGNORE INTO users VALUES(?, ?, ?, ?)', (tid, user_name, first_name, last_name))
+    base.commit()
+    base.close()
