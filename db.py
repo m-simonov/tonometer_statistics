7cae40f89e1f6661720a7750d9078860128bfee9
simonovmaksim <simonov.maksim13@yandex.ru>
2021-10-25 13:59:18 +0300

    Added update and count meterings

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import Dict
+from typing import Dict, List
 
 from aiogram import types
 
@@ -36,3 +36,29 @@ def insert(table_name: str, column_values: Dict):
     )
     base.commit()
     base.close()
+
+def update_metering(user: str, date: str, column: str, value: str):
+    base = sqlite3.connect('tonometer.db')
+    cur = base.cursor()
+
+    cur.execute(
+        "UPDATE meterings "
+        f"SET {column} == ? "
+        "WHERE user == ? AND date == ?",
+        (value, user, date)
+    )
+    base.commit()
+    base.close()
+
+def count_meterings(user: str, date: str):
+    base = sqlite3.connect('tonometer.db')
+    cur = base.cursor()
+
+    selected = cur.execute(
+        "SELECT "
+        "COUNT(morning), COUNT(afternoon), COUNT(evening) "
+        "FROM meterings "
+        "WHERE user == ? AND date == ?",
+        (user, date)
+    ).fetchone()
+    return sum(selected)
--- handlers.py
+++ handlers.py
@@ -15,24 +15,37 @@ async def print_info(message: types.Message):
     db.add_user(message)
 
 @dp.message_handler()
-async def wtite_results(message: types.Message):
+async def write_results(message: types.Message):
     msk_time = datetime.datetime.now(pytz.timezone('Europe/Moscow'))
     hour = int(msk_time.strftime('%H'))
     date_now = msk_time.date()
     if 0 < hour < 12:
-        time = 'morning'
+        column = 'morning'
     elif 12 < hour < 18:
-        time = 'afternoon'
+        column = 'afternoon'
     else:
-        time = 'evening'
+        column = 'evening'
 
-    db.insert(
-        'meterings',
-        {
-            'user': message.from_user.id,
-            'date': date_now,
-            time: message.text,
-        }
-    )
+    user = message.from_user.id
+    metering = message.text
 
-    await message.answer(f'{result}')
+    if db.count_meterings(user, date_now) == 3:
+        await message.answer('Все данные на сегодня заполнены')
+    elif db.count_meterings(user, date_now) == 0:
+        db.insert(
+            'meterings',
+            {
+                'user': user,
+                'date': date_now,
+                column: metering,
+            }
+        )
+    else:
+        db.update_metering(
+            user,
+            date_now,
+            column,
+            metering,
+        )
+
+    await message.answer(f'{metering}')
