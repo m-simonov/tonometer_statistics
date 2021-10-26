8f458880746f86e3e40f677afc59df62e3e9f70c
simonovmaksim <simonov.maksim13@yandex.ru>
2021-10-26 21:00:37 +0300

    Added regexp filter for write_metering

--- handlers.py
+++ handlers.py
@@ -1,10 +1,7 @@
-import datetime
-import sqlite3
-
-import pytz
 from aiogram import types
 
 import db
+import metering
 from main import bot, dp
 
 
@@ -14,38 +11,15 @@ async def print_info(message: types.Message):
     await message.answer(text=text)
     db.add_user(message)
 
-@dp.message_handler()
-async def write_results(message: types.Message):
-    msk_time = datetime.datetime.now(pytz.timezone('Europe/Moscow'))
-    hour = int(msk_time.strftime('%H'))
-    date_now = msk_time.date()
-    if 0 < hour < 12:
-        column = 'morning'
-    elif 12 < hour < 18:
-        column = 'afternoon'
-    else:
-        column = 'evening'
-
+@dp.message_handler(regexp=r"^([1-9]\d{1,2}) ([1-9]\d{1,2}) ([1-9]\d{1,2})$")
+async def write_metering(message: types.Message):
     user = message.from_user.id
-    metering = message.text
+    metering_result = message.text
+    column = metering.determine_the_time()
 
-    if db.count_meterings(user, date_now) == 3:
-        await message.answer('Все данные на сегодня заполнены')
-    elif db.count_meterings(user, date_now) == 0:
-        db.insert(
-            'meterings',
-            {
-                'user': user,
-                'date': date_now,
-                column: metering,
-            }
-        )
-    else:
-        db.update_metering(
-            user,
-            date_now,
-            column,
-            metering,
-        )
+    text = metering.write_to_db(user, metering_result, column)
+    await message.answer(text)
 
-    await message.answer(f'{metering}')
+@dp.message_handler()
+async def wrong_message(message: types.Message):
+    await message.answer('Некорректный формат ввода')
