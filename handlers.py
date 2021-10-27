b6f5777c527ab6fbc3e9f705387e78b41a8c3e41
simonovmaksim <simonov.maksim13@yandex.ru>
2021-10-27 17:44:37 +0300

    Added today_results command

--- handlers.py
+++ handlers.py
@@ -1,4 +1,6 @@
+from datetime import date
 from aiogram import types
+from aiogram.types import message_auto_delete_timer_changed
 
 import db
 import metering
@@ -10,6 +12,20 @@ async def print_info(message: types.Message):
     text = "Hello"
     await message.answer(text=text)
     db.add_user(message)
+
+@dp.message_handler(commands=['today_results'])
+async def show_today_meterings(message: types.Message):
+    user = message.from_user.id
+    date = message.date.date()
+    results = db.read_meterings_by_date(user, date)
+    morning = results[0]
+    afternoon = results[1]
+    evening = results[2]
+    show_results = (
+        f"Дата: {date}\n\n"
+        f"Утро: {morning}\nДень: {afternoon}\nВечер: {evening}"
+    )
+    await message.answer(show_results)
 
 @dp.message_handler(regexp=r"^([1-9]\d{1,2}) ([1-9]\d{1,2}) ([1-9]\d{1,2})$")
 async def write_metering(message: types.Message):
