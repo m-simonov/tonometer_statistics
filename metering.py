855c1938e9f4d5d70cc3a9c33f79936fba4da291
simonovmaksim <simonov.maksim13@yandex.ru>
2021-10-28 12:21:05 +0300

    Fixed write_to_db and determine_the_time

--- metering.py
+++ metering.py
@@ -4,17 +4,15 @@ import db
 def determine_the_time(hour: int):
     if 0 < hour < 12:
         column = 'morning'
-    elif 12 < hour < 18:
+    elif 12 <= hour < 18:
         column = 'afternoon'
     else:
         column = 'evening'
     return column
 
 def write_to_db(user: str, date: str, column: str, metering_result: str):
-    number_of_meterings = 3 - db.read_meterings_by_date(user, date).count(None)
-    if number_of_meterings == 3:
-        text = 'Все данные на сегодня заполнены'
-    elif number_of_meterings == 0:
+    today_meterings = db.read_meterings_by_date(user, date)
+    if not today_meterings:
         db.insert(
             'meterings',
             {
@@ -23,7 +21,9 @@ def write_to_db(user: str, date: str, column: str, metering_result: str):
                 column: metering_result,
             }
         )
-        text = f"Результат утреннего замера '{metering_result}' записан"
+        text = f"Результат замера '{metering_result}' записан"
+    elif 3 - today_meterings.count(None) == 3:
+        text = 'Все данные на сегодня заполнены'
     else:
         db.update_metering(
             user,
