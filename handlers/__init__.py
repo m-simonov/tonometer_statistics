5ddc47e366575456d05aa0d8d561b14d65152c26
simonovmaksim <simonov.maksim13@yandex.ru>
2021-11-01 14:16:08 +0300

    Added handlers package

---
+++ handlers/__init__.py
@@ -0,0 +1,4 @@
+from .admin import dp
+from .client import dp
+
+__all__ = ["dp"]
---
+++ handlers/admin.py
@@ -0,0 +1,23 @@
+import os
+
+from aiogram import types
+from dotenv import load_dotenv
+
+import db
+from main import bot, dp
+
+load_dotenv()
+admin_id = os.environ.get('admin_id')
+def auth_admin(func):
+    async def wrapper(message):
+        if str(message.from_user.id) not in admin_id:
+            return await message.answer(text='Access Denied')
+        return await func(message)
+    return wrapper
+
+@dp.message_handler(commands=['ppl'])
+@auth_admin
+async def show_users(message: types.Message):
+    users = db.read_users()
+    text = '\n'.join(map(str, enumerate(users, 1)))
+    await message.answer(text)
