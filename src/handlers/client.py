1b002bd13644760fc01a76a028255153be618747
Maksim <simonov.maksim13@yandex.ru>
2024-02-01 23:30:54 +0300

    Test deploy

--- src/handlers/client.py
+++ src/handlers/client.py
@@ -5,11 +5,10 @@ import db
 import matplotlib.pyplot as plt
 import metering
 from aiogram import types
-from aiogram.types.base import InputFile
 from aiogram.types.callback_query import CallbackQuery
 from keybords.inline.callback_data import by_month_callback
 from keybords.inline.choice_buttons import by_month
-from main import bot, dp
+from main import dp
 
 
 @dp.message_handler(commands=['start'])
@@ -29,12 +28,14 @@ async def print_info(message: types.Message):
     await message.answer(text=text)
     db.add_user(message)
 
+
 @dp.message_handler(commands=['today_results'])
 async def show_today_meterings(message: types.Message):
     user = message.from_user.id
     date = message.date.date()
     text = metering.show_today_meterings(user, date)
     await message.answer(text)
+
 
 @dp.message_handler(commands=['this_month_results'])
 async def show_this_months_meterings(message: types.Message):
@@ -44,7 +45,8 @@ async def show_this_months_meterings(message: types.Message):
     month = date.strftime('%m')
 
     text = metering.show_monthly_meterings(user, year, month)
-    await message.answer(text)    
+    await message.answer(text)
+
 
 @dp.message_handler(commands=['by_month_results'])
 async def by_month_command(message: types.Message):
@@ -53,18 +55,20 @@ async def by_month_command(message: types.Message):
         reply_markup=by_month
         )
 
+
 @dp.callback_query_handler(by_month_callback.filter())
 async def show_meterings_by_month(call: CallbackQuery, callback_data: dict):
     await call.answer(cache_time=2)
     user = call.from_user.id
     month = callback_data.get("month")
     year = call.message.date.strftime('%Y')
-    
+
     text = metering.show_monthly_meterings(user, year, month)
     if text:
         await call.message.answer(text=text)
     else:
         await call.message.answer(text="Нет данных")
+
 
 @dp.message_handler(commands=['show_month_graph'])
 async def show_month_graph(message: types.Message):
@@ -97,7 +101,7 @@ async def show_month_graph(message: types.Message):
             morning_sys.append(0)
             morning_dia.append(0)
             morning_pulse.append(0)
-    
+
     bar = plt.bar(days, morning_sys)
     plt.title("This month morning results")
     plt.xlabel("Days")
@@ -114,6 +118,7 @@ async def show_month_graph(message: types.Message):
 
     await message.answer_photo(photo=img)
 
+
 @dp.message_handler(regexp=r"^([1-9]\d{1,2}) ([1-9]\d{1,2}) ([1-9]\d{1,2})$")
 async def write_metering(message: types.Message):
     user = message.from_user.id
@@ -123,6 +128,7 @@ async def write_metering(message: types.Message):
     column = metering.determine_the_time()
     text = metering.write_to_db(user, date, column, metering_result)
     await message.answer(text)
+
 
 @dp.message_handler()
 async def wrong_message(message: types.Message):
