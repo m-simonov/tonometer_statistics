0885b1709e881c21675c3079e2056e3691847124
Maksim <simonov.maksim13@yandex.ru>
2024-02-05 23:33:20 +0300

    TS-1 Meterings renamed to measurement

--- src/db/models/measurement.py
+++ src/db/models/measurement.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from db.models.base import AbstractModel
 
 
-class Meterings(AbstractModel):
+class Measurement(AbstractModel):
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
     morning: Mapped[str] = mapped_column(String(11))
