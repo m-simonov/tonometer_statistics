6ac85a63aa9f8b807bbd3890a6e9aeddc06eb184
Maksim <simonov.maksim13@yandex.ru>
2024-02-13 00:07:46 +0300

    TS-1 Changed connection

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,19 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from db.models.base import AbstractModel
 
 
 DSN__DATABASE = "sqlite+aiosqlite:///tonometer.db"
 
 
 engine = create_async_engine(DSN__DATABASE, echo=True)
-async_session = sessionmaker(
+async_session_maker = sessionmaker(
     engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
-
-
-async def get_session():
-    async with async_session() as session:
-        yield session
