fb8635311fdc352a39a1e5617f8d0f03e0e22b46
Maksim <simonov.maksim13@yandex.ru>
2024-02-12 23:42:00 +0300

    TS-1 Changed field naming and relations

--- src/db/models/measurement.py
+++ src/db/models/measurement.py
@@ -7,7 +7,7 @@ from db.models.base import AbstractModel
 
 
 class Measurement(AbstractModel):
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped[int] = mapped_column(ForeignKey('user.tid'))
     date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
     morning: Mapped[str] = mapped_column(String(11))
     afternoon: Mapped[str] = mapped_column(String(11))
--- src/db/models/user.py
+++ src/db/models/user.py
@@ -5,7 +5,7 @@ from db.models.base import AbstractModel
 
 
 class User(AbstractModel):
-    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=True)
+    tid: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=True)
     username: Mapped[str] = mapped_column(String(32), unique=True)
     first_name: Mapped[str] = mapped_column(String(32))
     last_name: Mapped[str] = mapped_column(String(32))
