7112a70243d7a27079fb8a95028cd206944e2938
Maksim <simonov.maksim13@yandex.ru>
2024-02-14 00:28:32 +0300

    TS-1 Added nullable=True to User model fields

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -7,5 +7,5 @@ from db.models.base import AbstractModel
 class User(AbstractModel):
     tid: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=True)
     username: Mapped[str] = mapped_column(String(32), unique=True)
-    first_name: Mapped[str] = mapped_column(String(32))
-    last_name: Mapped[str] = mapped_column(String(32))
+    first_name: Mapped[str] = mapped_column(String(32), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(32), nullable=True)
