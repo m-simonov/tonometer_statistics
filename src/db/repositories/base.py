6732bf958a4c63e213d5836d8e585116463f3d0f
Maksim <simonov.maksim13@yandex.ru>
2024-02-16 00:24:50 +0300

    TS-1 Added args kwargs in filters

--- src/db/repositories/base.py
+++ src/db/repositories/base.py
@@ -15,12 +15,21 @@ class AbstractRepository:
     async def add(self, batch: List[model]):
         self.session.add_all(batch)
 
-    async def get(self, **kwargs):
-        stmt = select(self.model).filter_by(**kwargs)
+    async def get(self, *args, **kwargs):
+        if args:
+            stmt = select(self.model).filter(*args)
+        elif kwargs:
+            stmt = select(self.model).filter_by(**kwargs)
+        else:
+            raise ValueError
         result = await self.session.scalar(stmt)
         return result
 
-    async def list(self):
+    async def list(self, *args, **kwargs):
         stmt = select(self.model)
+        if args:
+            stmt = stmt.filter(*args)
+        else:
+            stmt = stmt.filter_by(**kwargs)
         result = await self.session.scalars(stmt)
         return result.all()
