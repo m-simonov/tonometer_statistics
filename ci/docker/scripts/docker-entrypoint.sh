0bc084b4cb0271ae152a28876d7d1d91e0368fef
Maksim <simonov.maksim13@yandex.ru>
2024-02-17 01:09:26 +0300

    TS-3 Added alembic upgrade head in entrypoint

--- ci/docker/scripts/docker-entrypoint.sh
+++ ci/docker/scripts/docker-entrypoint.sh
@@ -2,26 +2,6 @@
 
 set -e
 
-# Указываем имя файла SQL-запросов
-sql_file="create_db.sql"
-
-# Указываем имя базы данных SQLite
-db_name="tonometer.db"
-
-# Проверяем, существует ли файл SQL-запросов
-if [ ! -f "$sql_file" ]; then
-    echo "Файл $sql_file не найден."
-    exit 1
-fi
-
-# # Выполняем SQL-запросы к базе данных SQLite
-# sqlite3 "$db_name" < "$sql_file"
-
-# # Проверяем код возврата sqlite3
-# if [ $? -eq 0 ]; then
-#     echo "SQL-запросы успешно выполнены."
-# else
-#     echo "Ошибка выполнения SQL-запросов."
-# fi
+alembic upgrade head
 
 exec "$@"
