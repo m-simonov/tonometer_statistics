58215709b1c1a76731909ef3f3e63faf9c17cf53
Maksim <simonov.maksim13@yandex.ru>
2024-02-17 20:44:46 +0300

    Added User.tid not null

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -5,7 +5,7 @@ from db.models.base import AbstractModel
 
 
 class User(AbstractModel):
-    tid: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=True)
+    tid: Mapped[int] = mapped_column(BigInteger, unique=True)
     username: Mapped[str] = mapped_column(String(32), unique=True)
     first_name: Mapped[str] = mapped_column(String(32), nullable=True)
     last_name: Mapped[str] = mapped_column(String(32), nullable=True)
