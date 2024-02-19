08ff8106ae2bfc44f80cd5a1cb87e9af772e2c56
Maksim <simonov.maksim13@yandex.ru>
2024-02-19 22:33:35 +0300

    TS-8 Added unique constraint for measurement.user and measurement.date

--- src/db/models/measurement.py
+++ src/db/models/measurement.py
@@ -2,13 +2,18 @@ import datetime
 
 from sqlalchemy import Date, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.schema import UniqueConstraint
 
 from db.models.base import AbstractModel
 
 
 class Measurement(AbstractModel):
     user: Mapped[int] = mapped_column(ForeignKey('user.tid'))
-    date: Mapped[datetime.date] = mapped_column(Date, unique=True, nullable=False)
+    date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
     morning: Mapped[str] = mapped_column(String(11), nullable=True)
     afternoon: Mapped[str] = mapped_column(String(11), nullable=True)
     evening: Mapped[str] = mapped_column(String(11), nullable=True)
+
+    __table_args__ = (
+        UniqueConstraint('user', 'date', name='unique_user_date'),
+    )
