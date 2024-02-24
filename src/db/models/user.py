590cf8e4536d8fa6bd04cfe28de89daeeaf8f121
Maksim <simonov.maksim13@yandex.ru>
2024-02-24 12:04:30 +0300

    Hotfix user username made nullable true

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -6,6 +6,6 @@ from db.models.base import AbstractModel
 
 class User(AbstractModel):
     tid: Mapped[int] = mapped_column(BigInteger, unique=True)
-    username: Mapped[str] = mapped_column(String(32), unique=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=True)
     first_name: Mapped[str] = mapped_column(String(32), nullable=True)
     last_name: Mapped[str] = mapped_column(String(32), nullable=True)
